9e50584ba16beb7ea2eb440d76ce4066c2e82a11
noah <noah.lee@buzzvil.com>
2021-09-01 22:52:10 +0900

    Add Header to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Root, Routes, addPrefetchExcludes } from 'react-static'
-//
+import { 
+  Row, 
+  Col,
+  Button,
+} from 'antd'
+
 import { Link, Router } from 'components/Router'
+import { Header, Content } from 'components/Layout'
+import HeaderLink from 'components/HeaderLink'
 import Dynamic from 'containers/Dynamic'
 
-import './app.css'
 import './app.less'
 
 // Any routes that start with 'dynamic' will be treated as non-static routes
@@ -13,20 +19,50 @@ addPrefetchExcludes(['dynamic'])
 function App() {
   return (
     <Root>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/dynamic">Dynamic</Link>
-      </nav>
-      <div className="content">
+      <Header>
+        <Row>
+          <Col
+            lg={12}>
+            Logo
+          </Col>
+          <Col 
+            lg={{span: 3}}
+          >
+            <HeaderLink>
+              <Link to="/">Home</Link>
+            </HeaderLink>
+          </Col>
+          <Col 
+            lg={{span: 3}}
+          >
+            <HeaderLink>
+              <Link to="/about">About</Link>
+            </HeaderLink>
+          </Col>
+          <Col 
+            lg={{span: 3}}
+          >
+            <HeaderLink>
+              <Link to="/blog">Blog</Link>
+            </HeaderLink>
+          </Col>
+          <Col 
+            lg={{span: 3}}
+          >
+            <HeaderLink>
+              <Link to="/dynamic">Dynamic</Link>
+            </HeaderLink>
+          </Col>
+        </Row>
+      </Header>
+      <Content>
         <React.Suspense fallback={<em>Loading...</em>}>
           <Router>
             <Dynamic path="dynamic" />
             <Routes path="*" />
           </Router>
         </React.Suspense>
-      </div>
+      </Content>
     </Root>
   )
 }
