38626637eb89152217caac363a58147a66bcf27a
noah <noah.lee@buzzvil.com>
2021-09-15 20:11:00 +0900

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { Root, Routes, addPrefetchExcludes } from 'react-static'
 import { 
-  Row, 
-  Col,
   Typography
 } from 'antd'
 
-import { Link, Router } from 'components/Router'
-import { Header, Content, Footer } from 'components/Layout'
-import HeaderLink from 'components/HeaderLink'
+import { Router } from 'components/Router'
+import Header from 'components/Header'
+import { Content, Footer } from 'components/Layout'
 import Dynamic from 'containers/Dynamic'
 
 import './app.less'
@@ -21,42 +19,7 @@ addPrefetchExcludes(['dynamic'])
 function App() {
   return (
     <Root>
-      <Header>
-        <Row>
-          <Col
-            lg={12}>
-            Logo
-          </Col>
-          <Col 
-            lg={{span: 3}}
-          >
-            <HeaderLink>
-              <a href="https://cloud.gitploy.io/">Cloud</a>
-            </HeaderLink>
-          </Col>
-          <Col 
-            lg={{span: 3}}
-          >
-            <HeaderLink>
-              <a href="https://doc.gitploy.io/">Docs</a>
-            </HeaderLink>
-          </Col>
-          <Col 
-            lg={{span: 3}}
-          >
-            <HeaderLink>
-              <Link to="/about">Pricing</Link>
-            </HeaderLink>
-          </Col>
-          <Col 
-            lg={{span: 3}}
-          >
-            <HeaderLink>
-              <a href="https://github.com/gitploy-io/gitploy/discussions">Community</a>
-            </HeaderLink>
-          </Col>
-        </Row>
-      </Header>
+      <Header />
       <Content>
         <React.Suspense fallback={<em></em>}>
           <Router>
