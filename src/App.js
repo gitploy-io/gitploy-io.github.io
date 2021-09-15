0c0e002d5b4bbbc59ed43dc3f109e9ad642a4fc9
noah <noah.lee@buzzvil.com>
2021-09-15 20:17:30 +0900

    Add Footer component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import { Root, Routes, addPrefetchExcludes } from 'react-static'
-import { 
-  Typography
-} from 'antd'
 
 import { Router } from 'components/Router'
 import Header from 'components/Header'
-import { Content, Footer } from 'components/Layout'
+import Footer from 'components/Footer'
+import { Content } from 'components/Layout'
 import Dynamic from 'containers/Dynamic'
 
 import './app.less'
-
-const { Text } = Typography
 
 // Any routes that start with 'dynamic' will be treated as non-static routes
 addPrefetchExcludes(['dynamic'])
@@ -28,11 +24,7 @@ function App() {
           </Router>
         </React.Suspense>
       </Content>
-      <Footer>
-        <div style={{textAlign: "center", margin: "50px 0"}}>
-          <Text type="secondary">© 2021 Gitploy.io, Inc All rights reserved.</Text>
-        </div>
-      </Footer>
+      <Footer />
     </Root>
   )
 }
