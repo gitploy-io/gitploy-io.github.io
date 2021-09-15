4b04e12a5d5a4f4f31f6ffa5b3d3aba88033fdf9
noah <noah.lee@buzzvil.com>
2021-09-15 21:34:27 +0900

    Simplify CodeHighliter

--- src/components/CodeHighliter.js
+++ src/components/CodeHighliter.js
@@ -4,16 +4,40 @@ import {
   Col, 
 } from 'antd'
 
-export default function CodeHighliter(props) {
+export default function CodeHighliter() {
+  const lines=[
+    <p className="gitploy-yaml-code">
+      <span className="gitploy-yaml-code-key">envs: </span>
+    </p>,
+    <p className="gitploy-yaml-code">
+      <span className="gitploy-yaml-code-key">- name: </span><span className="gitploy-yaml-code-value"> prod</span>
+    </p>,
+    <p className="gitploy-yaml-code">
+      <span className="gitploy-yaml-code-key">&nbsp;&nbsp; auto_merge: </span><span className="gitploy-yaml-code-value"> false</span>
+    </p>,
+    <p className="gitploy-yaml-code">
+      <span className="gitploy-yaml-code-key">&nbsp;&nbsp; required_contexts: </span><span className="gitploy-yaml-code-value"> ['lint', 'test']</span>
+    </p>,
+    <p className="gitploy-yaml-code">
+      <span className="gitploy-yaml-code-key">&nbsp;&nbsp; approval: </span>
+    </p>,
+    <p className="gitploy-yaml-code">
+      <span className="gitploy-yaml-code-key">&nbsp;&nbsp; - enabled: </span> <span className="gitploy-yaml-code-value"> true</span>
+    </p>,
+    <p className="gitploy-yaml-code">
+      <span className="gitploy-yaml-code-key">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; required_count: </span> <span className="gitploy-yaml-code-value"> 1</span>
+    </p>,
+  ]
+
   return <div className="gitploy-yaml-block">
 		<Row>
 			<Col className="gitploy-yaml-divider" span={1}>
-				{props.lines.map((_, idx) => {
+				{lines.map((_, idx) => {
 					return <p key={idx} className="gitploy-yaml-line-number">{idx+1}</p>
 				})}
 			</Col>
 			<Col span={22}>
-				{props.lines.map((value, idx) => {
+				{lines.map((value, idx) => {
 					return <div key={idx}>{value}</div>
 				})}
 			</Col>
--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ export default () => (
       </Row>
       <Row >
         <Col span={22} offset={1} md={{span: 8, offset: 8}} style={{textAlign: "center"}}>
-          <Title type="secondary" level={5}>Gitploy is the easiest way to build an advanced deployment pipeline.</Title>
+          <Title type="secondary" level={5}>Gitploy is the easiest way to build an advanced deployment system.</Title>
         </Col>
       </Row>
       <Row style={{marginTop: "20px"}}>
@@ -50,84 +50,31 @@ export default () => (
       </Row>
     </div>
     <Row style={{ marginTop: "150px"}}>
-      <Col offset={1} span={22} lg={{span: 14, offset: 5}}>
+      <Col offset={1} span={22} lg={{span: 16, offset: 4}}>
         <Row>
           <Col span={24} md={{span: 11}}>
-            <Title style={{color: colorDeepPurple}} level={2}>Configuration as code</Title>
-            <Title type="secondary" level={5}>Pipelines are configured with a simple, easy‑to‑read file that you commit to your git repository.</Title>
-            <Title type="secondary" level={5}>Build an advanced deployment pipeline from scratch in minutes.</Title>
+            <Title style={{color: colorDeepPurple}} level={3}>Configuration as code</Title>
+            <p><Text type="secondary">Pipeline is configured with a simple, easy‑to‑read file that you commit to your git repository.</Text></p>
+            <p><Text type="secondary">Build an advanced deployment pipeline from scratch in minutes.</Text></p>
           </Col>
           <Col span={24} md={{span: 11, offset: 2}}>
-            <CodeHighliter 
-              lines={[
-                <p className="gitploy-yaml-code">
-                  <span className="gitploy-yaml-code-key">envs: </span>
-                </p>,
-                <p className="gitploy-yaml-code">
-                  <span className="gitploy-yaml-code-key">- name: </span><span className="gitploy-yaml-code-value"> prod</span>
-                </p>,
-                <p className="gitploy-yaml-code">
-                  <span className="gitploy-yaml-code-key">&nbsp;&nbsp; auto_merge: </span><span className="gitploy-yaml-code-value"> false</span>
-                </p>,
-                <p className="gitploy-yaml-code">
-                  <span className="gitploy-yaml-code-key">&nbsp;&nbsp; required_contexts: </span><span className="gitploy-yaml-code-value"> ['lint', 'test']</span>
-                </p>,
-                <p className="gitploy-yaml-code">
-                  <span className="gitploy-yaml-code-key">&nbsp;&nbsp; approval: </span>
-                </p>,
-                <p className="gitploy-yaml-code">
-                  <span className="gitploy-yaml-code-key">&nbsp;&nbsp; - enabled: </span> <span className="gitploy-yaml-code-value"> true</span>
-                </p>,
-                <p className="gitploy-yaml-code">
-                  <span className="gitploy-yaml-code-key">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; required_count: </span> <span className="gitploy-yaml-code-value"> 1</span>
-                </p>,
-              ]}
-            />
+            <CodeHighliter />
           </Col>
         </Row>
       </Col>
     </Row>
-    {/* <div style={{ marginTop: "150px"}} >
-      <Row>
-        <Col span={24}>
-          <div style={{textAlign: "center", marginBottom: "50px"}}>
-            <Title style={{color: colorDeepPurple}} level={2}>Features</Title>
-          </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col span={24} md={8}>
-          <div style={{textAlign: "center", padding: "10px"}}>
-            <Image width={110} src={RocketIcon} preview={false}/>
-            <Title style={{color: colorDeepPurple}} level={3}>Deploy</Title>
-            <Text type="secondary">Click the commit, branch, or tag in the Gitploy to deploy your code. It doesn't get any simpler than that.</Text>
-          </div>
-        </Col>
-        <Col span={24} md={8}>
-          <div style={{textAlign: "center", padding: "10px"}}>
-            <Image width={110} src={RocketIcon} preview={false}/>
-            <Title style={{color: colorDeepPurple}} level={3}>Rollback</Title>
-            <Text type="secondary">Click the release you want to rollback in the Gitploy. It is the best way to recover while you fix the problems.</Text>
-          </div>
-        </Col>
-        <Col span={24} md={8}>
-          <div style={{textAlign: "center", padding: "10px"}}>
-            <Image width={110} src={RocketIcon} preview={false}/>
-            <Title style={{color: colorDeepPurple}} level={3}>Promote</Title>
-            <Text type="secondary">Click the release from one evironment in the Gitploy. Promote saves you the trouble of bugs.</Text>
-          </div>
-        </Col>
-      </Row>
-    </div> */}
     <Row style={{ marginTop: "150px"}}>
-      <Col offset={1} span={22} lg={{span: 14, offset: 5}}>
+      <Col offset={1} span={22} lg={{span: 16, offset: 4}}>
         <Row>
-          <Col span={24} md={{span: 11}}>
-            <Title style={{color: colorDeepPurple}} level={2}>Powered By Github</Title>
-            <Title type="secondary" level={5}>Gitploy dispatches a Github deployment event that external services can listen and act.</Title>
-            <Title type="secondary" level={5}>You can build loosely coupled tooling around deployments.</Title>
+          <Col span={0} md={{span: 11}}>
+            <Image src={GithubLogo} preview={false}/>
           </Col>
           <Col span={24} md={{span: 11, offset: 2}}>
+            <Title style={{color: colorDeepPurple}} level={3}>Powered By Github</Title>
+            <p><Text type="secondary">It's an event-driven decoupled way to deploy your code.</Text></p>
+            <p><Text type="secondary">You don't need to worry about the implementation details of deploying different types of applications.</Text></p>
+          </Col>
+          <Col span={24} md={0}>
             <Image src={GithubLogo} preview={false}/>
           </Col>
         </Row>
