53e92ea73db22e98a2ce7b6e34032b67c6d86c0b
noah <noah.lee@buzzvil.com>
2021-09-28 12:05:53 +0900

    Align deploy form to center

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ const { Text } = Typography
 
 export default function Footer() {
   return (
-    <div style={{textAlign: "center", margin: "50px 0"}}>
-      <Text type="secondary">© 2021 Gitploy.io, Inc All rights reserved.</Text>
+    <div style={{textAlign: "center"}}>
+      <Text type="secondary" style={{padding: "50px 0"}}>© 2021 Gitploy.io, Inc All rights reserved.</Text>
     </div>
   )
 }
