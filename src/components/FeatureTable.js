a9b89aa895012df2731fba397ce7110672807dc5
Noah Hanjun Lee <noah.lee@buzzvil.com>
2021-09-28 11:20:40 +0900

    Add pricing page (#4)
    * Fix style of ref
    
    * Add pricing page

---
+++ src/components/FeatureTable.js
@@ -0,0 +1,32 @@
+import React from "react"
+import { Table } from "antd"
+import { CheckOutlined } from '@ant-design/icons';
+
+export default function FeaturesTable(props) {
+  const columns = [
+    {
+      title: "FEATURE",
+      dataIndex: "feature",
+      key: "feature",
+      align: "left"
+    },
+    {
+      title: "ENTERPRISE",
+      dataIndex: "enterprise",
+      key: "enterprise",
+      align: "center",
+      render: (ok) => (ok)? <CheckOutlined /> : null
+    },
+  ]
+
+  return (
+    <Table 
+      columns={columns}
+      dataSource={props.datas.map((data, idx) => {
+        const d =  {...data, key: idx}
+        return d
+      })} 
+      pagination={false}
+    />
+  )
+}
